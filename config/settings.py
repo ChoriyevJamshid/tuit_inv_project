553143aa279fbfe0b6cd6f098b6df4b65026915f
root <root@vmi1828860.contaboserver.net>
2024-05-07 18:49:06 +0200

    fix: config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+$fhl4gxzl771qf06@nmzouz*4dhu!k+nwynl^6#d^c%lkd&b8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
